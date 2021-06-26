d39b1d55950c9d3cfddf9d74cb56553168c54bd6
KSHyun9692 <KSHyun9692@gmail.com>
2021-06-26 22:03:34 +0900

    JS연습

--- momentum/app.js
+++ momentum/app.js
@@ -129,41 +129,69 @@
 // console.log(title1);
 // console.log(title2);
 
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//    h1.style.color = "blue";
+// }
+
+// function handleMouseEnter(){
+//    h1.innerText = "mouse is here!";
+// }
+
+// function handleMouseLeave(){
+//    h1.innerText = "Mouse is gone!";
+// }
+
+// function handleWindowResize(){
+//    document.body.style.backgroundColor = "tomato";
+// }
+
+// function handleWindowCopy(){
+//    alert("copier");
+// }
+
+// function handleWindowOffline(){
+//    alert("SOS no WIFI");
+// }
+// function handleWindowOnline(){
+//    alert("ALL GOOD");
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+// h1.addEventListener("mouseenter", handleMouseEnter);
+// h1.addEventListener("mouseleave", handleMouseLeave);
+
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
+
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//    const currentColor = h1.style.color;
+//    let newColor;
+//    if(currentColor === "blue"){
+//       newColor = "tomato";
+//    }else{
+//       newColor = "blue";
+//    }
+//    h1.style.color = newColor;
+// }
+
+// h1.addEventListener("click", handleTitleClick);
 
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-   h1.style.color = "blue";
-}
-
-function handleMouseEnter(){
-   h1.innerText = "mouse is here!";
-}
-
-function handleMouseLeave(){
-   h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-   document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-   alert("copier");
-}
-
-function handleWindowOffline(){
-   alert("SOS no WIFI");
-}
-function handleWindowOnline(){
-   alert("ALL GOOD");
+   // const clickedClass = "clicked";
+   // if(h1.classList.contains(clickedClass)){
+   //    h1.classList.remove(clickedClass);
+   // }else{
+   //    h1.classList.add(clickedClass);
+   // }
+   h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
