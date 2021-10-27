1cc1680c38579c3fed0c07c72f5e95196490b4d1
KSHyun9692 <KSHyun9692@gmail.com>
2021-10-27 19:58:41 +0900

    JS기초연습!

---
+++ STUDY/01_JS/7.js/lecture09.js
@@ -0,0 +1,16 @@
+var a;
+
+a = 1;
+console.log(++a);
+console.log(a);
+
+a = 1;
+console.log(a++);
+console.log(a);
+
+/**
+ * Math 관련 명령어
+Math.pow(A,b) : A의 B승을 구해 줌
+Math.sqrt(A) : A의 제곱근을 구해 줌
+Math.random() : 0~1 사이의 임의의 난수를 발생시켜 줌
+ */
